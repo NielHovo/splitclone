af6bae88b124df21e454e69e711a7a363a62884b
Niels Hovorka <nhovorka.mmt-b2021@fh-salzburg.ac.at>
2023-06-23 10:22:55 +0200

    Added further frontend, finished index page, started with navigation

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,27 @@
+'use client';
 import React from 'react';
 import supabase from './supabaseConnection';
+import Link from 'next/link';
 
 export default async function Posts() {
   let users = [
-    { id: 1, username: 'Hyusoko', balance: -47.4 },
+    { id: 1, username: 'Hyusoko', balance: -35.4 },
     { id: 2, username: 'Biene', balance: 0 },
     { id: 3, username: 'BepisLoca', balance: 35.4 }
   ];
+  let transactions = [
+    { id: 1, userId: 3, amount: 15, title: "Venue Fees" },
+    { id: 2, userId: 3, amount: 57.4, title: "Food" },
+    { id: 3, userId: 2, amount: 37, title: "2-Day-Ticket" },
+    { id: 4, userId: 1, amount: 1.6, title: "Croissant" },
+  ]
+
+  function handleTransactionClick(trans) {
+    history.push({
+      pathname: "/bill",
+      state: { transaction: trans }
+    });
+  }
 
   return (
     <div className="w-[600px] flex flex-col justify-center items-center">
@@ -14,15 +29,37 @@ export default async function Posts() {
       <h2 className="w-[600px]">Overview</h2>
       <ul className="w-[600px]">
         {users.map((user) => (
-          <li className="w-full flex justify-between p-2" key={user.id}>
-            <p>{user.username}</p>
-            <p className={user.balance < 0 ? 'text-red-600' : (user.balance > 0 ? 'text-green-600' : 'text-black')}>
-              {user.balance >= 0 ? "+" + user.balance.toFixed(2) : user.balance.toFixed(2)}€
-            </p>
-          </li>
+          <a href='./bill'>
+            <li
+              className="w-full flex justify-between p-2"
+              key={user.id}
+              onClick={() => handleTransactionClick(trans)}
+            >
+              <p>{user.username}</p>
+              <p className={user.balance < 0 ? 'text-red-600' : (user.balance > 0 ? 'text-green-600' : 'text-black')}>
+                {user.balance >= 0 ? "+" + user.balance.toFixed(2) : user.balance.toFixed(2)}€
+              </p>
+            </li>
+          </a>
         ))}
       </ul>
-      <button className='custom-btn btn-15 mt-5'>Add user</button>
+      <button className='custom-btn btn-15 m-5'>Add user</button>
+      <ul className="w-[600px]">
+        {transactions.map((trans) => (
+          <Link href={`/bill/${trans.id}`}>
+            <li className="w-full flex justify-between expense items-center cursor-pointer p-2" key={trans.id}>
+              <div>
+                <p className='font-bold'>{trans.title}</p>
+                <p>{users.find((user) => user.id === trans.userId)?.username}</p>
+              </div>
+              <p>
+                {trans.amount.toFixed(2)}€
+              </p>
+            </li>
+          </Link>
+        ))}
+      </ul>
+      <button className='custom-btn btn-15 m-5'>Add Expense</button>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,8 @@ export default function Navbar() {
     return (
         <nav className='fixed top-0 left-0 flex justify-center text-white font-bold bg-black p-4 w-full'>
             <div className='flex justify-between w-[600px]'>
-                <a href=''>User Vinz</a>
-                <a href=''>Log Out</a>
+                <a className='specialA' href=''>User Hyusoko</a>
+                <a className='specialA' href=''>Log Out</a>
             </div>
         </nav>
     )
