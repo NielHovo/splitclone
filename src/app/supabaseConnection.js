39019f69c1af5b31b651d114144797f3aa2f5476
Niels Hovorka <nhovorka.mmt-b2021@fh-salzburg.ac.at>
2023-07-18 12:51:55 +0200

    Deployment issues

--- src/app/supabaseConnection.js
+++ src/app/supabaseConnection.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export default createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBrdmt4eGtjbXRiemNib2F0ZG9yIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODc0NTI4NjgsImV4cCI6MjAwMzAyODg2OH0.wrLtZrHMRTeGKt9aAAJJZiq1pnAlPS_YJs6iEmEj-_g",
+  "//pkvkxxkcmtbzcboatdor.supabase.co"
 )
